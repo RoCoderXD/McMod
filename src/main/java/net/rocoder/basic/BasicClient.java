c2a6615909179a2e12bbf868d4b7cdaf2b1d228c
RoCoderXD <exlgamingyt@gmail.com>
2022-12-11 12:50:34 -0600

    Initial commit

---
+++ src/main/java/net/rocoder/basic/BasicClient.java
@@ -0,0 +1,11 @@
+package net.rocoder.basic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
